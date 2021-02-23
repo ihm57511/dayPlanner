a63a60fdc9e9c20092592ae9b2aff551fb086079
ian mcbrayer <mcbrayer.ian@gmail.com>
2021-02-23 10:01:26 -0500

    code is complete, need to complete readme

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,6 @@ var today = moment();
 $("#currentDay").text(today.format("dddd, MMM Do"));
 
 var currentHour = parseInt(moment().format('H'));
-console.log(currentHour);
-
-var task = []
 
 function init() {
     presentInfo();
@@ -14,12 +11,12 @@ var presentInfo = function () {
     
     for (i = 0; i < 9; i++) {
     var timeEl = $('textarea').eq(i);
-    
-    // timeEl.text(JSON.parse(localStorage.getItem("task"[i])));
-        
+    console.log(timeEl)
+    $(timeEl).val(localStorage.getItem("task" + i));
+   
     if (parseInt(timeEl.attr("id")) < currentHour) {
         timeEl.addClass(' past');
-        console.log(parseInt(timeEl.attr("id")));
+       
     } 
     else if (parseInt(timeEl.attr("id")) === currentHour) {
         timeEl.addClass(' present');
@@ -29,13 +26,11 @@ var presentInfo = function () {
     }
 }};
 
+
 $('.saveBtn').click(function(event) {
     event.preventDefault();
-    var btnClicked = $(event.target);
-    console.log($(btnClicked).attr("id"));
-    console.log($(btnClicked).parent().siblings("textarea").val());
-    localStorage.setItem("task", JSON.stringify($(btnClicked).parent().siblings("textarea").val()));
-    return
+    var btnClicked = $(this);
+    localStorage.setItem("task" + [parseInt($(btnClicked).attr("id"))], $(btnClicked).parent().siblings("textarea").val());
 });
 
 init();
