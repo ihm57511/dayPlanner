66b559ed1d27dc522864b0b5ec3892ec58540aa5
ian mcbrayer <mcbrayer.ian@gmail.com>
2021-02-22 22:50:53 -0500

    corrected js to utilize jQuery, localStorage still inop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,68 +1,44 @@
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMM Do"));
 
-var currentHour = moment().format('H');
+var currentHour = parseInt(moment().format('H'));
 console.log(currentHour);
-// **** bug here ****
-// only first save button is operational, 
-// see line 55 for potential solution
-var saveButton = document.getElementById("button-addon2")
+
+var task = []
 
 function init() {
-    presentTime();
-    getTaskList();
-}
-
-var presentTime = function () {
-// **** bug here *****
-// does not hit id=9 and i do not understand why
-    for (i = 9; i <= 17; i++) {
-        if (document.getElementById([i]).id < currentHour) {
-            document.getElementById(i).className += " past";
-            console.log(document.getElementById([i]));
-        } 
-        else if (document.getElementById([i]).id === currentHour) {
-            document.getElementById(i).className += " present";
-            console.log(document.getElementById([i]));
-        }
-        else {
-            document.getElementById(i).className += " future";
-            console.log(document.getElementById([i]));
-        }
-    } return
-}
-
-var taskList = function() {
-    for (i = 9; i <= 17; i++) {
-        localStorage.setItem(i, document.getElementById([i]).value);
-        // if below line is run here, storage is not pulled upon page refresh
-        // document.getElementById(i).value = localStorage[i];
-    }
-
-    
+    presentInfo();
 };
 
-// ***** bug here *****
-// returns undefined if page loaded with cleared storag
-var getTaskList = function() {
-    for (i = 9; i <= 17; i++) {
-        document.getElementById(i).value = localStorage[i];
+var presentInfo = function () {
+    
+    for (i = 0; i < 9; i++) {
+    var timeEl = $('textarea').eq(i);
+    
+    // timeEl.text(JSON.parse(localStorage.getItem("task"[i])));
+        
+    if (parseInt(timeEl.attr("id")) < currentHour) {
+        timeEl.addClass(' past');
+        console.log(parseInt(timeEl.attr("id")));
+    } 
+    else if (parseInt(timeEl.attr("id")) === currentHour) {
+        timeEl.addClass(' present');
+    } 
+    else {
+        timeEl.addClass(' future');
     }
-}
+}};
 
-saveButton.addEventListener("click", taskList); 
-console.log(document.getElementById([9]).value)
-
-// **********
-// unsure of how to structure 
-// variable.addEventListener("click", function(event) {
-//     var element = event.target;
-
-//     if (element.matches("button") === true) {
-//         for (i = 9; i <= 17; i++) {
-//             localStorage.setItem(i, document.getElementById([i]).value);
-//         }return
-//     }
-// });
+$('.saveBtn').click(function(event) {
+    event.preventDefault();
+    var btnClicked = $(event.target);
+    console.log($(btnClicked).attr("id"));
+    console.log($(btnClicked).parent().siblings("textarea").val());
+    localStorage.setItem("task", JSON.stringify($(btnClicked).parent().siblings("textarea").val()));
+    return
+});
 
 init();
+
+    
+
